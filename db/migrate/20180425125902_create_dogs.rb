6d22879384adc973e82c3b4a3b279786a0807b16
Frank McCallion <fkmccallion@gmail.com>
2018-04-25 13:04:11 +0000

    automatically backed up by learn

--- db/migrate/20180425125902_create_dogs.rb
+++ db/migrate/20180425125902_create_dogs.rb
@@ -1,5 +1,12 @@
 class CreateDogs < ActiveRecord::Migration
 
+  #def change ## Change method performs the same way as the combined up and down methods
+  #  create_table :dogs do |t|
+  #    t.string :name
+  #    t.string :breed
+  #  end
+  #end
+  
   def up
     create_table :dogs do |t|
       t.string :name
